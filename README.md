17254d4fef2a2ac81d441de01d13cfc91628fd82
PradatiusD <danielprada2012@gmail.com>
2013-07-29 16:49:06 -0400

    Added support forIE6,7,8,9 for contact form, mailchimp sample email, as well as partially fixed renedring of navigation on IE8 and IE7.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Alain Pupo
+
+### Documentation
+This responsive WordPress theme is powered by the following frameworks:
+* Twitter Bootstrap (v 2.3.1)
+* [SASS](http://sass-lang.com)
+* [Bourbon.io](http://bourbon.io)
+* [Basic jQuery Sldier](http://www.basic-slider.com/)
+* [Thesis Framework](http://diythemes.com/)
--- js/main.js
+++ js/main.js
@@ -35,4 +35,29 @@ jQuery(document).ready(function($){
 	$(window).resize(function(){
 		dropdownz();
 	})
+
+
+	// Function to get widget data and pass it to the #events div
+
+	// Get the number of total events
+	var allEvents = $('.widget.widget_text.apeventwidget');
+	var numEvents = allEvents.length;
+
+	// Cycle through each event and compose the correct html
+	// for the events section, and then push it to the APevents array.
+	var APevents = [];
+
+	for (var i = 0; i < numEvents; i++) {
+		var eventH3 = "<h3>"+ allEvents.eq(i).find('h3').text()+"</h3>";
+		var eventH4 = "<h4>"+ allEvents.eq(i).find('h4').text()+"</h4>";
+		var eventStrong = "<strong>"+ allEvents.eq(i).find('strong').text()+"</strong>";
+		var eventP = "<p>"+ allEvents.eq(i).find('p').text()+"</p>";
+		// Now compose the correct html and push to the AP events array
+		var EventHTML = "<li><div class='left'>"+eventH3+eventH4+"</div><div class='right'>"+eventStrong+eventP+"</div></li>";
+		APevents.push(EventHTML);
+	};
+
+	for (var i = 0; i < APevents.length; i++) {
+		$("#events").find('ul').append(APevents[i])
+	};
 })
