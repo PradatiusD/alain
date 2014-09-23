03f9b0cca7b32e2e1399d86d6e30b09d7ae0f822
PradatiusD <danielprada2012@gmail.com>
2014-09-23 18:17:22 -0400

    Added basic facebook feed via php sdk.  Need to handle as many likes as possible and get bigger images

--- theme/functions.php
+++ theme/functions.php
@@ -1,4 +1,8 @@
 <?php
+// For Debugging
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
 // Register Custom Navigation Walker
 require_once('lib/wp_bootstrap_navwalker.php');
 
@@ -14,3 +18,38 @@ function register_menus() {
   register_nav_menu('right-menu',__( 'Right Menu' ));
 }
 add_action( 'init', 'register_menus' );
+
+
+// Facebook Feed
+require 'lib/facebook.php';
+$facebook = new Facebook(array(
+  'appId'  => '324143354431454',
+  'secret' => '90a2b06a5a792d89638d439b2a774db3',
+));
+
+function fb_feed() {
+
+	$jsonFilePath = 'fb.json';
+
+	if (file_exists($jsonFilePath)) {
+
+		$jsonFile = fopen($jsonFilePath, 'r');
+		$data = fread($jsonFile, filesize($jsonFilePath));
+		fclose($jsonFile);
+
+	} else {
+
+		global $facebook;
+		
+		$request = $facebook->api('/488298457891169/feed');
+
+		// turn data into JSON
+		$data = json_encode($request);
+
+		// Write to file
+		$jsonFile = fopen($jsonFilePath, "w");
+		fwrite($jsonFile, $data);
+		fclose($jsonFile);
+	}
+	return $data;
+}
--- theme/index.php
+++ theme/index.php
@@ -1,5 +1,33 @@
 <?php get_header();?>
-<div class="container">
-	<h1>Alain Pupo</h1>
+<div class="container-fluid" ng-app='alain'>
+  
+  <section class="row">
+  
+    <div ng-controller="FacebookController" class="col-md-4 col-md-offset-4" ng-show="fbData">
+        <div ng-repeat="post in fbData" ng-show="post.message">
+          <h3>{{post.from.name}} <small class="text-muted">{{post.updated_time | date:'MMM d, y h:mm a'}}</small></h3>
+          <p class="lead">{{post.message}}</p>
+          <p class="text-muted">Likes {{post.likes.data.length}} | Comentarios {{post.comments.data.length}}</strong>
+          <img ng-src="{{post.picture}}" class="img-responsive">
+          <hr>
+        </div>
+    </div>
+  
+  </section>
 </div>
+
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+<script>
+  var fbData = <?php echo fb_feed();?>;
+</script>
+
+<script>
+var App = angular.module('alain',[]);
+
+App.controller('FacebookController', function($scope) {
+  $scope.fbData = fbData.data;
+  console.log(fbData);
+});
+</script>
+
 <?php get_footer();?>
